b391166668857e8510d29897a28c16cf4e7abe72
Sainath <sainathreddynarahari6@gmail.com>
2018-03-23 17:46:04 -0500

    ForLoopLoop

---
+++ ForLoop/src/ForLoop.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : ForLoop.cpp
+// Author      : Sainath
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	for (int i = 0; i < 10; ++i) {
+		cout << "!!!Hello World!!!" << i << endl;
+	}
+
+	return 0;
+}
