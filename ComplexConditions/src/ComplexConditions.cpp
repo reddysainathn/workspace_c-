a09583904b4537cb66123707782d14d841a71b0f
Sainath <sainathreddynarahari6@gmail.com>
2018-03-23 17:37:52 -0500

    CompelxConditions

---
+++ ComplexConditions/src/ComplexConditions.cpp
@@ -0,0 +1,32 @@
+//============================================================================
+// Name        : ComplexConditions.cpp
+// Author      : Sainath
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+/*
+ * == equality test
+ * != not equals
+ * < less than
+ * > greater than
+ * <= less than or equal to
+ * >= greater than or equal to
+ * && Logical AND Condition
+ * || Logical OR Condition
+ */
+int main() {
+
+	int value1 = 7;
+	int value2 = 4;
+	if (value1 < 8) {
+		cout << "Condition1: true" << endl;
+	} else {
+		cout << "Condition1: false" << endl;
+	}
+
+	return 0;
+}
