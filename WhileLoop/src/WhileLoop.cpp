31886c1bf738463f68f2b15c7fe2d8501593da9a
Sainath <sainathreddynarahari6@gmail.com>
2018-03-23 17:41:02 -0500

    WhileLoop

---
+++ WhileLoop/src/WhileLoop.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : WhileLoop.cpp
+// Author      : Sainath
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i = 0;
+	while (i < 10) {
+		cout << "Printing! " << i << endl;
+		i++;
+	}
+	return 0;
+}
