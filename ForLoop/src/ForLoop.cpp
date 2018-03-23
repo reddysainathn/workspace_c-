8d52a158583647184ab8edb17046272bd6435ea4
Sainath <sainathreddynarahari6@gmail.com>
2018-03-23 17:50:16 -0500

    ForLoop with Continue and Break.

--- ForLoop/src/ForLoop.cpp
+++ ForLoop/src/ForLoop.cpp
@@ -11,7 +11,14 @@ using namespace std;
 
 int main() {
 	for (int i = 0; i < 10; ++i) {
-		cout << "!!!Hello World!!!" << i << endl;
+		cout << "i is :" << i << endl;
+		if (i == 3) {
+			break; // Break will break all iterations after this point.
+		}
+		if (i == 2) {
+			continue; // Continue will not execute the statements after countinue and executes the next iteration.
+		}
+		cout << "Looping.." << endl;
 	}
 
 	return 0;
